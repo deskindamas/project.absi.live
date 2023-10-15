fb671004e0cce476b215dd6490e53937ca791ba9
unknown <naremanrh21@gmail.com>
2023-10-15 10:25:38 +0300

    abo khaled

--- pages/seller/store/index.js
+++ pages/seller/store/index.js
@@ -8,7 +8,37 @@ import Storeimage from '../../../public/images/storeimage.jpg';
 
 
 const Store = () => {
-
+ 
+    const categories = [
+      {
+        id : 1 ,
+        name : 'lorem1'
+      },
+      {
+        id : 2 ,
+        name : 'lorem2'
+      },
+      {
+        id : 3 ,
+        name : 'lorem3'
+      },
+      {
+        id : 4 ,
+        name : 'lorem4'
+      },
+      {
+        id : 5,
+        name : 'lorem5'
+      },
+      {
+        id : 6 ,
+        name : 'lorem6'
+      },
+      {
+        id : 7 ,
+        name : 'lorem7'
+      },
+    ]
 
   const [users, setUsers] = useState([]);
   const getUsers = async () => {
@@ -29,6 +59,17 @@ const Store = () => {
                 <Image className='shadow absolute z-10 top-6 left-10 rounded-[50%]' src={Logo} alt='store'/>
                 <h1>Super Star</h1>
                 </div>
+              </div>
+              <div className=''>
+              <ul>
+              {
+                categories.map((categorie) => {
+                return (
+                  <li key = {categorie.id}> {categorie.name} </li>
+                   )
+                })
+                }
+              </ul>
               </div>
               <div className="grid md:grid-cols-3 sm:grid-cols-1 grid-col-1 gap-4 ">
               {
