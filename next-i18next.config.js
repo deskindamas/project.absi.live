437ad294f1b9239684627befadee547dfa451483
Mahmood Haraba <mahoodharaba@gmail.com>
2023-11-10 21:40:29 +0300

    translation fix

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,10 +1,11 @@
-// const path = require("path");
+const path = require("path");
 
 module.exports = {
   i18n: {
     locales: ["en", "ar"],
     defaultLocale: "en" ,
     localeDetection: false,
-    localePath: "./public/locales",
+    // localePath: "./public/locales",
   },
+  localePath: path.resolve("./public/locales"),
 };
